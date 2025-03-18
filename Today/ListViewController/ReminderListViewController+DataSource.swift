bf2684fc5f4f963111e7bc6eed8fbb3ba0652bf0
MarlzRana <77016115+MarlzRana@users.noreply.github.com>
2025-03-18 21:10:00 +0000

    Add a Non-Functional Done Button to Reminder List

--- Today/ListViewController/ReminderListViewController+DataSource.swift
+++ Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -13,15 +13,40 @@ extension ReminderListViewController {
     
     func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
         let reminder = Reminder.sampleData[indexPath.item]
-        var contentConfiguratiion = cell.defaultContentConfiguration()
-        contentConfiguratiion.text = reminder.title
-        contentConfiguratiion.secondaryText = reminder.dueDate.dayAndTimeText
-        contentConfiguratiion.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
-        cell.contentConfiguration = contentConfiguratiion
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = reminder.title
+        contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
+        contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
+        cell.contentConfiguration = contentConfiguration
+        
+        var doneButtonConfiguration = doneButtonConfiguration(for: reminder)
+        doneButtonConfiguration.tintColor = .todayListCellDoneButtonTint
+        cell.accessories = [
+            .customView(configuration: doneButtonConfiguration),
+            .disclosureIndicator(displayed: .always)
+        ]
         
         var backgroundConfiguration = UIBackgroundConfiguration.listCell()
         backgroundConfiguration.backgroundColor = .todayListCellBackground
         cell.backgroundConfiguration = backgroundConfiguration
+    }
+    
+    private func doneButtonConfiguration(for reminder: Reminder) -> UICellAccessory.CustomViewConfiguration {
+        let symbolName = reminder.isComplete ? "circle.fill" : "circle"
+        let symbolConfiguration = UIImage.SymbolConfiguration(textStyle: .title1)
+        
+        let image = UIImage(systemName: symbolName, withConfiguration: symbolConfiguration)
+        let highlightedImage = UIImage(systemName: "circle.circle", withConfiguration: symbolConfiguration)
+        
+        let button = UIButton()
+        button.setImage(image, for: .normal)
+        button.setImage(highlightedImage, for:.highlighted)
+        
+        return UICellAccessory.CustomViewConfiguration(
+            customView: button,
+            placement: .leading(displayed: .always)
+        )
+        
     }
     
     
