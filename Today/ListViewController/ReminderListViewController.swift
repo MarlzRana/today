49c0c09e5407f622aacd7d9db2529bf094f025fe
MarlzRana <77016115+MarlzRana@users.noreply.github.com>
2025-04-17 22:29:29 +0100

    Make Filter Picker Functional

--- Today/ListViewController/ReminderListViewController.swift
+++ Today/ListViewController/ReminderListViewController.swift
@@ -47,6 +47,7 @@ class ReminderListViewController: UICollectionViewController {
         }
         
         listStyleSegmentedControl.selectedSegmentIndex = listStyle.rawValue
+        listStyleSegmentedControl.addTarget(self, action: #selector(didChangeListStyle(_:)), for: .valueChanged)
         
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didPressAddButton(_:)))
         addButton.accessibilityLabel = NSLocalizedString("Add reminder", comment: "Add button accessibility label")
